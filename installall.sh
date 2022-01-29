c781ca85a5c3502d8c3e4e1346518374650ba9b6
Subhamoy Banerjee <60469025+pheonix-03@users.noreply.github.com>
2022-01-29 09:13:47 +0530

    Add files via upload

---
+++ installall.sh
@@ -0,0 +1,17 @@
+sudo apt install default-jdk
+sudo apt install default-jre
+sudo apt install timeshift
+sudo apt install python3-pip
+sudo apt install code
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+
+sudo apt-get install apt-transport-https
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt install sublime-text
+sudo apt install vim
+sudo apt install sqlite3
+sudo apt install onlyoffice-desktopeditors
+sudo apt install cherrytree
+sudo add-apt-repository ppa:tomtomtom/conky-manager
+sudo apt update && sudo apt install conky-manager2
+sudo apt install vlc
